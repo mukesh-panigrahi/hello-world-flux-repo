8960e98441b708ec04c8fc7b7136db980184d7b1
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-12-25 14:11:50 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,10 +2,10 @@
 
 #!/bin/sh
 
-git config --global user.name kartikey.sarode
-git config --global user.email kartikey.sarode@searce.com
+#git config --global user.name kartikey.sarode
+#git config --global user.email kartikey.sarode@searce.com
 git remote add origin git@github.com:mukesh-panigrahi/hello-world-flux-repo.git
-git checkout -b test-3
+git checkout -b test-5
 git add .
-git commit -m 'test-3'
-git push origin test-3
+git commit -m 'test-5'
+git push origin test-5
