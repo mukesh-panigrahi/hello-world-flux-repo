368c54d724a5d90110a5703ad0c59750275e6cca
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-12-27 13:48:59 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@
 
 git remote add origin git@github.com:mukesh-panigrahi/hello-world-flux-repo.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-git checkout -b test-99
+git checkout -b test-123
 git add .
-git commit -m 'test-99'
-git push origin test-99
+git commit -m 'test-123'
+git push origin test-123
